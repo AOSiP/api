d2fc871fabcdfda2d9247b80a0e2b1256eea5df5
Akhil Narang <akhilnarang.1999@gmail.com>
2018-02-21 00:21:43 +0530

    updater: gen_mirror_json: update for AOSiP
      * our zipname is formatted differently, adjust according to that
      * Check for only .zip files, we have others in there too sometimes
    
    Change-Id: I0613934b0a590a9cfbcfe1b2af422126be00495d
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- gen_mirror_json.py
+++ gen_mirror_json.py
@@ -15,19 +15,21 @@ builds = {}
 for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(FILE_BASE) for f in fn]:
     data = open(f).read()
     filename = f.split('/')[-1]
-    # lineage-14.1-20171129-nightly-hiaeul-signed.zip
-    _, version, builddate, buildtype, device = os.path.splitext(filename)[0].split('-')
-    print('hashing sha256 for {}'.format(filename), file=sys.stderr)
-    sha256 = hashlib.sha256(data).hexdigest()
-    builds.setdefault(device, []).append({
-        'sha256': hashlib.sha256(data).hexdigest(),
-        'size': os.path.getsize(f),
-        'date': '{}-{}-{}'.format(builddate[0:4], builddate[4:6], builddate[6:8]),
-        'filename': filename,
-        'filepath': f.replace(FILE_BASE, ''),
-        'version': version,
-        'type': buildtype.lower()
-    })
+    if filename.split('.')[-1] == 'zip':
+        # lineage-14.1-20171129-nightly-hiaeul-signed.zip
+        _, version, buildtype, device, builddate = os.path.splitext(filename)[0].split('-')
+        print('hashing sha256 for {}'.format(filename), file=sys.stderr)
+        sha256 = hashlib.sha256(data).hexdigest()
+        builds.setdefault(device, []).append({
+            'sha256': hashlib.sha256(data).hexdigest(),
+            'size': os.path.getsize(f),
+            'date': '{}-{}-{}'.format(builddate[0:4], builddate[4:6], builddate[6:8]),
+            'filename': filename,
+            'filepath': f.replace(FILE_BASE, ''),
+            'version': version,
+            'type': buildtype.lower()
+        })
+
 for device in builds.keys():
     builds[device] = sorted(builds[device], key=lambda x: x['date'])
 print(json.dumps(builds, sort_keys=True, indent=4))
