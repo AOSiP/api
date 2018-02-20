897e55cfaf6ad8b2afb3f74190c8dd5de35a0d77
Akhil Narang <akhilnarang.1999@gmail.com>
2018-02-21 01:48:39 +0530

    updater: config: update for AOSiP
      * Link our json and downloads site
        (We aren't going to be using mirrorbits)
    
      * Remove gerrit and redis related stuff - we aren't using it
    
    Change-Id: I170755bb334e9dfb6b846be069f507b1f026564a
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
 
 class Config(object):
-    GERRIT_URL = os.environ.get('GERRIT_URL', 'https://review.lineageos.org')
-    CACHE_TYPE = os.environ.get('CACHE_TYPE', 'simple')
-    CACHE_REDIS_HOST = os.environ.get('CACHE_REDIS_HOST', 'redis')
-    CACHE_REDIS_DB = os.environ.get('CACHE_REDIS_DB', 4)
-    WIKI_INSTALL_URL = os.environ.get('WIKI_INSTALL_URL', 'https://wiki.lineageos.org/devices/{device}/install')
-    WIKI_INFO_URL = os.environ.get('WIKI_INFO_URL', 'https://wiki.lineageos.org/devices/{device}')
 
-    UPSTREAM_URL = os.environ.get('UPSTREAM_URL', '')
-    DOWNLOAD_BASE_URL = os.environ.get('DOWNLOAD_BASE_URL', 'https://mirrorbits.lineageos.org')
+    UPSTREAM_URL = os.environ.get('UPSTREAM_URL', 'https://ota.aosiprom.com/builds.json')
+    DOWNLOAD_BASE_URL = os.environ.get('DOWNLOAD_BASE_URL', 'https://get.aosiprom.com/')
