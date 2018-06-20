13ac9560bb136434fd74785c09f444fce6ed8cd7
Akhil Narang <akhilnarang.1999@gmail.com>
2018-06-21 01:12:09 +0530

    updater: gen_mirror_json: work only against the latest zip
      * This is probably horrible, but it works (for now)
      * Hardcode the base directory, since it's a constant for us
      * Use glob.glob() to list the devices and only Official zips
      * Sort the output and take the latest zip from it, work only on that
      * In the case of an empty directory, do nothing
    
    Change-Id: I2d946f72fbdb091e3c72e1c863176689d74898c9
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- gen_mirror_json.py
+++ gen_mirror_json.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import hashlib
+import glob
 import json
 import os
 import sys
 
-if len(sys.argv) < 2:
-    print("usage python {} /path/to/mirror/base/url".format(sys.argv[0]))
-    sys.exit()
-
-FILE_BASE = sys.argv[1]
+FILE_BASE = "/home/kronic/get.aosiprom.com"
 builds = {}
-
-for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(FILE_BASE) for f in fn]:
-    data = open(f).read()
-    filename = f.split('/')[-1]
-    if filename.split('.')[-1] == 'zip' and filename.split('-')[0] == 'AOSiP':
-        # lineage-14.1-20171129-nightly-hiaeul-signed.zip
-        _, version, buildtype, device, builddate = os.path.splitext(filename)[0].split('-')
-        print('hashing sha256 for {}'.format(filename), file=sys.stderr)
-        sha256 = hashlib.sha256(data).hexdigest()
-        builds.setdefault(device, []).append({
-            'sha256': hashlib.sha256(data).hexdigest(),
-            'size': os.path.getsize(f),
-            'date': '{}-{}-{}'.format(builddate[0:4], builddate[4:6], builddate[6:8]),
-            'filename': filename,
-            'filepath': f.replace(FILE_BASE, ''),
-            'version': version,
-            'type': buildtype.lower()
-        })
+cwd=os.getcwd()
+os.chdir(FILE_BASE)
+for f in sorted(glob.glob('*')):
+    if f[0] != '.' and f[0] != '_':
+        os.chdir(f)
+        try:
+            filename=sorted(glob.glob('*Official*.zip'))[-1]
+            _, version, buildtype, device, builddate = os.path.splitext(filename)[0].split('-')
+            print('hashing sha256 for {}'.format(filename), file=sys.stderr)
+            builds.setdefault(device, []).append({
+                'sha256': hashlib.sha256(open(filename).read()).hexdigest(),
+                'size': os.path.getsize(filename),
+                'date': '{}-{}-{}'.format(builddate[0:4], builddate[4:6], builddate[6:8]),
+                'filename': filename,
+                'filepath': '/{}/{}'.format(f, filename),
+                'version': version,
+                'type': buildtype.lower()
+            })
+        except IndexError:
+            continue
+        finally:
+            os.chdir('../')
 
 for device in builds.keys():
     builds[device] = sorted(builds[device], key=lambda x: x['date'])
 print(json.dumps(builds, sort_keys=True, indent=4))
+os.chdir(cwd)
