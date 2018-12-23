4036149730d89c98deab82591f62b82ab34dd4be
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-24 03:31:35 +0530

    api: fix logic
    This will just give us the last element in the sorted list, i.e the
    latest zip in that folder
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-DIR = '/var/www/aosiprom.com/beta'
+DIR = '/var/www/aosiprom.com/beta/*'
 
 @app.route('/')
 def show_files():
     zips = []
     for d in iglob(DIR):
-        f = glob(d).sort()[-1]
-        if f.split('.')[-1] == 'zip':
-            zip = f.split('/')[-1]
+
+        f = glob(d + '/*.zip').sort()
+        if f:
+            zip = f[-1]
             zips.append(zip)
     zips.sort()
     return render_template('latest.html', zips=zips)
