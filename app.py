5294c431f7bbd788e6de76ece1e399b2d4cb3378
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-24 02:57:14 +0530

    api: miscellaneous updates
    Keep SF and OSDN base URLs in a variable
    Just pass zip name to template, grab device from it
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
-
 import os
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_files():
-    data = []
+    zips = []
     for f in [os.path.join(dp, f) for dp, dn, fn in os.walk('/var/www/aosiprom.com/beta') for f in fn]:
         if f.split('.')[-1] == 'zip':
-            n = f.split('/')
-            data.append(n[-2]+'/'+n[-1])
-    data.sort()
-    return render_template('latest.html', data=data)
+            zip = f.split('/')[-1]
+            zips.append(zip)
+    zips.sort()
+    return render_template('latest.html', zips=zips)
 
 
 if __name__ == '__main__':
