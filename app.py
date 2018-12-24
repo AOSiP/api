ae0861d8b2f467648db5ca7f246dc11463ca6134
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-24 19:07:34 +0530

    api: go back to os.walk() for getting the zips
    Store the zips in a dict with the codename as key
    Only store the latest zip in the list
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from glob import glob, iglob
+import os
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-DIR = '/var/www/aosiprom.com/beta/*'
+DIR = os.getenv('DIR', '/var/www/aosiprom.com/beta')
+
+
+def get_date_from_zip(zip):
+    return zip.split('-')[-1].split('.')[0]
+
 
 @app.route('/')
 def show_files():
-    zips = []
-    for d in iglob(DIR):
-        f = glob(d + '/*.zip')
-        if f:
-            f.sort()
-            zip = f[-1].split('/')[-1]
-            zips.append(zip)
-    zips.sort()
-    return render_template('latest.html', zips=zips)
+    zips = {}
+    for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(DIR) for f in fn]:
+        zip = f[-1].split('/')[-1]
+        device = zip.split('-')[3]
+        if zips[device]:
+            if get_date_from_zip(zips[device]) > get_date_from_zip(zip):
+                continue
+        zips[device] = zip
+    data = list(zips.values())
+    data.sort()
+    return render_template('latest.html', zips=data)
 
 
 if __name__ == '__main__':
