AOSiP/api

commit 083491bf4474b053cdec21d8baabba3266f1cbed

Yeah this just seems so much simpler, we don't need a full list returned for a single zip...
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

app.py
@@ -21,9 +21,7 @@ def get_zips(dir):
             if get_date_from_zip(zips[device]) > get_date_from_zip(zip):
                 continue
         zips[device] = zip
-    data = list(zips.values())
-    data.sort()
-    return data
+    return zips[device]
 
 @app.route('/')
 def show_files():

commit da76f0e2c51ecf75ca54a8a6cee6fffa3affd4d1

get_zips: fix returned data
This broke in commit 083491bf4474b053cdec21d8baabba3266f1cbed
I forgot about the / route

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

app.py
@@ -33,7 +33,9 @@ def get_zips(directory):
             if get_date_from_zip(zips[device]) > get_date_from_zip(zip_name):
                 continue
         zips[device] = zip_name
-    return zips[device]
+    data = list(zips.values())
+    data.sort()
+    return data
 
 
 @app.route('/')