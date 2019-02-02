d50c93fdafe62a04294eb25d898535220f9962be
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-02 19:18:20 +0530

    config: remove trailing slash
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
 
     UPSTREAM_URL = os.environ.get('UPSTREAM_URL', 'https://ota.aosiprom.com/builds.json')
-    DOWNLOAD_BASE_URL = os.environ.get('DOWNLOAD_BASE_URL', 'https://downloads.sourceforge.net/project/illusionproject/')
+    DOWNLOAD_BASE_URL = os.environ.get('DOWNLOAD_BASE_URL', 'https://downloads.sourceforge.net/project/illusionproject')
