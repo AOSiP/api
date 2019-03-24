1d43d6b083b0d686435fbf7d2d3b52e15810c5b9
Akhil Narang <akhilnarang.1999@gmail.com>
2019-03-24 21:46:38 +0530

    api: update jenkins script
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,6 @@
 PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-killall flask python3
+[[ -n $(pidof flask) ]] && killall flask
+[[ -n $(pidof python3) ]] && killall python3
+[[ -d "venv" ]] || python3 -m venv ./venv
+source venv/bin/activate
 ./app.py
