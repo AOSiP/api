18096b1c1a2e809ee36f559f143cd937d8e8057a
Akhil Narang <akhilnarang.1999@gmail.com>
2019-04-22 17:02:44 +0530

    api: install the latest requirements too
    CI seems to actually be working now, so why not
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,5 @@ PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 [[ -n $(pidof python3) ]] && killall python3
 [[ -d "venv" ]] || python3 -m venv ./venv
 source venv/bin/activate
+python3 -m pip install -r requirements.txt
 ./app.py
