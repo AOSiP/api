c0649fc00a51d60d2e3c407e4be99643eb9b51b8
Akhil Narang <akhilnarang.1999@gmail.com>
2019-12-15 23:24:14 +0530

    api: use python3.8
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 [[ -n $(pidof flask) ]] && killall flask
 [[ -n $(pidof python3) ]] && killall python3
-[[ -d "venv" ]] || python3 -m venv ./venv
+[[ -d "venv" ]] || python3.8 -m venv ./venv
 source venv/bin/activate
-python3 -m pip install -r requirements.txt
+python3.8 -m pip install -r requirements.txt
 echo "Starting up!"
 gunicorn app:app --workers=$(nproc)
