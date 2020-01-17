9a82575e7ba5082ad94a001bc139ffbb36cae366
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-17 12:08:03 +0530

    api: remove favicon route from flask, move it to the templates
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app.py
+++ app.py
@@ -148,13 +148,6 @@ def get_device_version(device):
 ##########################
 # API
 ##########################
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory(
-        os.path.join(app.root_path, "static"),
-        "favicon.ico",
-        mimetype="image/vnd.microsoft.icon",
-    )
 
 
 @app.route("/")
